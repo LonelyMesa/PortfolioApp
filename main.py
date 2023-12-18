180cc2ae9e6c56101740d1eb5941bc99896a6f4e
LonelyMesa <kenleon94@gmail.com>
2023-12-18 14:37:37 -0700

    Add description on the new columns

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     st.image("images/photo.png")
@@ -14,3 +14,9 @@ with col2:
     pursue a degree in computer science. 
      """
     st.info(content)
+
+content2 = """
+Below you find some of the apps I have built in Python. Feel free to contact me.
+"""
+st.write(content2)
+
